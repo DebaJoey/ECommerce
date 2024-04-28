25aa3641208931e17de54ffe74187ef9496b65e2
Joshua Debazar <debajoshua@gmail.com>
2024-04-28 23:32:32 +0000

    Seed Data Config  Added

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API.Data;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,5 +32,19 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    logger.LogError("An error occured during Migration");
+}
 
 app.Run();
